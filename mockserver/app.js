9dd06898e19af722c21f72e869d46a52e6a13316
Joost van Viegen <joost@fmvv.nl>
2020-12-16 16:20:31 +0100

    re-read data for each request

--- mockserver/app.js
+++ mockserver/app.js
@@ -1,14 +1,19 @@
 const http = require('http')
 
-const mockData = require('./mock.json')
-
+const fs = require('fs')
 const hostname = '127.0.0.1'
 const port = 3301
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify(mockData))
+    fs.readFile('./mock.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        res.end(data)
+    })
 })
 
 server.listen(port, hostname, () => {
