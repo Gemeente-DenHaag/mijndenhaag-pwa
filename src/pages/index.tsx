bebe81787bfdc0b7031abe111c77a3d83cf109da
DanielvanVliet <danielvvliet@hotmail.com>
2020-12-30 12:47:23 +0100

    gatsby setup with typescript starter

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PageProps } from 'gatsby';
+
+import Title from '@/components/Title';
+
+const Home: React.FC<PageProps> = () => (
+  <main>
+    <Title />
+  </main>
+);
+
+export default Home;
