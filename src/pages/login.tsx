c4d2455939a11fc1bdb5994dc34dbe81285d44f2
Bryan de Jong <bryandejong99@gmail.com>
2021-02-06 15:31:38 +0100

    Login page with request to mockserver

---
+++ src/pages/login.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {PageProps} from 'gatsby'
+import LoginSection from "@/components/LoginSection";
+
+const Login: React.FC<PageProps> = () => (
+    <main>
+        <LoginSection />
+    </main>
+)
+
+export default Login
