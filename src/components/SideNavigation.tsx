e7a535d53ed8c8e096493283913d89dddd5607d1
Maarten de Klerk <maarten@deklerk.dev>
2021-02-17 09:38:53 +0100

    Wrap list item in link

--- src/components/SideNavigation.tsx
+++ src/components/SideNavigation.tsx
@@ -1,35 +1,39 @@
 import React from 'react';
+import { Link } from "gatsby"
 
 import List from '@gemeente-denhaag/nlds-react-components/datadisplay/list/List';
 import ListItemIcon from '@gemeente-denhaag/nlds-react-components/datadisplay/listitemicon/ListItemIcon';
 import ListItem from '@gemeente-denhaag/nlds-react-components/datadisplay/listitem/ListItem';
 import ListItemText from '@gemeente-denhaag/nlds-react-components/datadisplay/listitemtext/ListItemText';
 
-
 import Icon, { HambugerIcon, ArchiveIcon, MapIcon, InboxIcon, FileTextIcon } from "./icons";
+
 interface NavigationItem {
   icon: React.FC;
   name: string;
+  to: string;
   color?: string;
 }
 
 const buildNavigation = (navigationItems: NavigationItem[]): React.ReactElement[] => {
-  return navigationItems.map<React.ReactElement>(item =>
-  (<ListItem>
-    <ListItemIcon><Icon icon={item.icon} color={item.color} /></ListItemIcon>
-    <ListItemText>{item.name}</ListItemText>
-  </ListItem>)
-  );
+  return navigationItems.map<React.ReactElement>(item => (
+    <Link to="#">
+      <ListItem>
+        <ListItemIcon><Icon icon={item.icon} color={item.color} /></ListItemIcon>
+        <ListItemText>{item.name}</ListItemText>
+      </ListItem>
+    </Link>
+  ));
 }
 
 const SideNavigation: React.FC = () => (
   <List>
     {buildNavigation([
-      { "name": "Overzicht", "icon": HambugerIcon, "color": "black" },
-      { "name": "Lopende zaken", "icon": ArchiveIcon, "color": "black" },
-      { "name": "Mijn producten", "icon": FileTextIcon, "color": "black" },
-      { "name": "Mijn wijk", "icon": MapIcon, "color": "black" },
-      { "name": "Meldingen", "icon": InboxIcon, "color": "black" }
+      { "name": "Overzicht", "icon": HambugerIcon, "to": "#", "color": "#4B4B4B" },
+      { "name": "Lopende zaken", "icon": ArchiveIcon, "to": "#", "color": "#4B4B4B" },
+      { "name": "Mijn producten", "icon": FileTextIcon, "to": "#", "color": "#4B4B4B" },
+      { "name": "Mijn wijk", "icon": MapIcon, "to": "#", "color": "#4B4B4B" },
+      { "name": "Meldingen", "icon": InboxIcon, "to": "#", "color": "#4B4B4B" }
     ])}
   </List>
 );
