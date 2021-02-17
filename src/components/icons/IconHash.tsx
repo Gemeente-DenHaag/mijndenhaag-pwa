b20588230ad22c5e42495d529458bda44c9b4e7e
Maarten de Klerk <maarten@deklerk.dev>
2021-02-17 10:17:46 +0100

    Update viewbox

--- src/components/icons/IconHash.tsx
+++ src/components/icons/IconHash.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import IIconProps from "@/components/icons/common";
 
 export const HashIcon: React.FC<IIconProps> = ({ color, ...props }) => (
-  <svg data-testid={`HashIcon`} fill='none' stroke={color} viewBox='0 0 20 20' xmlns="http://www.w3.org/2000/svg" {...props}>
+  <svg data-testid={`HashIcon`} fill='none' stroke={color} viewBox='0 0 24 24' xmlns="http://www.w3.org/2000/svg" {...props}>
     <path d="M4 9H20" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
     <path d="M4 15H20" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
     <path d="M10 3L8 21" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
