e694dbbb06f43f6aae335885625804151e36ccec
Maarten de Klerk <maarten@deklerk.dev>
2021-02-17 09:58:00 +0100

    Remove fill and stroke props from icons

--- src/components/icons/IconClock.tsx
+++ src/components/icons/IconClock.tsx
@@ -3,7 +3,7 @@ import IIconProps from "@/components/icons/common";
 
 export const ClockIcon: React.FC<IIconProps> = ({ color, ...props }) => (
   <svg data-testid={`ClockIcon`} fill='none' stroke={color} viewBox='0 0 20 20' xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#4B4B4B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-    <path d="M12 6V12L16 14" stroke="#4B4B4B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M12 6V12L16 14" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
   </svg>
 );
--- src/components/icons/IconSearch.tsx
+++ src/components/icons/IconSearch.tsx
@@ -3,7 +3,7 @@ import IIconProps from "@/components/icons/common";
 
 export const SearchIcon: React.FC<IIconProps> = ({ color, ...props }) => (
   <svg data-testid={`SearchIcon`} fill='none' stroke={color} viewBox='0 0 20 20' xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path d="M5.26667 9.53333C7.62308 9.53333 9.53333 7.62308 9.53333 5.26667C9.53333 2.91025 7.62308 1 5.26667 1C2.91025 1 1 2.91025 1 5.26667C1 7.62308 2.91025 9.53333 5.26667 9.53333Z" stroke="#4B4B4B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-    <path d="M12.9996 13.0001L8.59961 8.6001" stroke="#4B4B4B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M5.26667 9.53333C7.62308 9.53333 9.53333 7.62308 9.53333 5.26667C9.53333 2.91025 7.62308 1 5.26667 1C2.91025 1 1 2.91025 1 5.26667C1 7.62308 2.91025 9.53333 5.26667 9.53333Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M12.9996 13.0001L8.59961 8.6001" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
   </svg>
 );
