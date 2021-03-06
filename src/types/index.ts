ad938919b63bc35f936824284d01c503f80acd3b
Maarten de Klerk <maarten@deklerk.dev>
2021-03-06 13:43:36 +0100

    add types

---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+/**
+ * This file is just to alias the types in the schema.ts. To save some typing :P
+ */
+import { components } from './schema';
+
+export type Zaak = components["schemas"]["Zaak"];
+export type Status = components["schemas"]["Status"];
