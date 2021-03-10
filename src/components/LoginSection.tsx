207cc07b6c993bebffd0219242f814bd454f97f0
Bryan de Jong <bryandejong99@gmail.com>
2021-03-10 15:23:00 +0100

    Remove unnecessary state usage from login section

--- src/components/LoginSection.tsx
+++ src/components/LoginSection.tsx
@@ -1,23 +1,29 @@
-import TextField from "@gemeente-denhaag/nlds-react-components/input/textfield/TextField";
-import Button from "@gemeente-denhaag/nlds-react-components/input/button/Button";
-import React, {useState} from "react";
-import Grid from "@gemeente-denhaag/nlds-react-components/layout/grid/Grid";
-import {Typography} from "@gemeente-denhaag/nlds-react-components/datadisplay/typography/Typography";
-import {Container} from "@gemeente-denhaag/nlds-react-components/layout/Container/Container";
-import { navigate } from "gatsby";
+import Grid from '@gemeente-denhaag/grid/Grid'
+import TextField from '@gemeente-denhaag/textfield/TextField'
+import Container from '@gemeente-denhaag/container/Container'
+import {Typography} from "@gemeente-denhaag/datadisplay";
+import {Button} from "@gemeente-denhaag/input";
+
+import {navigate} from "gatsby";
+import React, {FormEvent} from "react";
 
 const LoginSection: React.FC = () => {
 
-    const [bsn, setBsn] = useState('680426280');
-    const [kvk, setKvk] = useState('80939155');
+    const submitBsn = (formSubmitEvent: FormEvent) => {
+        formSubmitEvent.preventDefault();
 
-    const submitBsn = () => {
-        //TODO: Submit BSN to API gateway to receive a JWT
+        const bsn = formSubmitEvent.target['bsn'].value;
+        //BSN should be sent to Gateway to receive JWT for mock login.
+
         navigate("/index");
     };
 
-    const submitKvk = () => {
-        //TODO: Submit Kvk to API gateway to receive a JWT
+    const submitKvk = (formSubmitEvent) => {
+        formSubmitEvent.preventDefault();
+
+        const kvk = formSubmitEvent.target['kvk'].value;
+        //KVK should be sent to Gateway to receive JWT for mock login.
+
         navigate("/index");
     };
 
@@ -26,16 +32,16 @@ const LoginSection: React.FC = () => {
             <Grid container spacing={1}>
                 <Grid item xs={12} md={6}>
                     <Typography variant={"h4"}>Inloggen met DigiD</Typography>
-                    <form>
-                        <TextField label={"BSN"} value={bsn} onChange={(event) => setBsn(event.target.value)}/>
-                        <Button variant={"contained"} color={"primary"} onClick={submitBsn}>Log in</Button>
+                    <form onSubmit={submitBsn}>
+                        <TextField label={"BSN"} name={"bsn"}/>
+                        <Button variant={"contained"} color={"primary"} type={"submit"}>Log in</Button>
                     </form>
                 </Grid>
                 <Grid item xs={12} md={6}>
                     <Typography variant={"h4"}>Inloggen met e-Herkenning</Typography>
-                    <form>
-                        <TextField label={"KVK"} value={kvk} onChange={(event) => setKvk(event.target.value)}/>
-                        <Button variant={"contained"} color={"primary"} onClick={submitKvk}>Log in</Button>
+                    <form onSubmit={submitKvk}>
+                        <TextField label={"KVK nummer"} name={"bsn"}/>
+                        <Button variant={"contained"} color={"primary"} type={"submit"}>Log in</Button>
                     </form>
                 </Grid>
             </Grid>
