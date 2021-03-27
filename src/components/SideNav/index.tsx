bb64dc7c020c60bc86da35ada02cef29f316d112
Maarten de Klerk <maarten@deklerk.dev>
2021-03-27 10:48:42 +0100

    remove styling

--- src/components/SideNav/index.tsx
+++ src/components/SideNav/index.tsx
@@ -20,37 +20,11 @@ interface NavigationItem {
   to: string
 }
 
-// TODO: Temporary styling, remove when design tokens is done.
-// cant set font weight, size and line height, its overriden by MUI
-const linkStyles = {
-  textDecoration: 'none',
-  fontStyle: 'normal',
-  fontWeight: 400,
-  fontSize: '16px',
-  lineHeight: '24px',
-  color: '#4B4B4B'
-}
-
-// TODO: Temporary styling, remove when design tokens is done.
-// cant set font weight, size and line height, its overriden by MUI
-const activeStyles = {
-  fontStyle: 'normal',
-  fontWeight: 500,
-  fontSize: '16px',
-  lineHeight: '24px',
-  color: '#1261A3'
-}
-
 const buildNavigation = (
   navigationItems: NavigationItem[]
 ): React.ReactElement[] => {
   return navigationItems.map<React.ReactElement>((item, i) => (
-    <Link
-      to={item.to}
-      style={linkStyles}
-      activeStyle={activeStyles}
-      partiallyActive={true}
-    >
+    <Link to={item.to} partiallyActive={true}>
       <ListItem key={i}>
         <Icon
           icon={item.icon}
